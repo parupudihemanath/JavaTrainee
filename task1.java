2463608656731e42bc18ef74c88523797446de0f
parupudihemanath <139130900+parupudihemanath@users.noreply.github.com>
2023-07-12 09:43:07 +0530

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,118 @@
+package onlinevideos;
+
+public class task1 
+{
+	 public static void main(String args[])
+	 {
+		 
+		 //task1
+		 System.out.println("task1");
+		 String message="That was great - lol.";
+		String change= message.replace("lol", "laugh out loud");
+		 System.out.println(change);
+		 System.out.println();
+		 
+		 //task 2
+		 System.out.println("task2");
+		 String capitals="ALEX";
+		    String Substrng= capitals.toLowerCase().substring(1);
+		     System.out.println(capitals.charAt(0)+ Substrng);
+		     System.out.println();
+		     
+		     String capital="ALEX";
+		     String result=capital.substring(0,1).toUpperCase().concat(capital.substring(1).toLowerCase());
+		     System.out.println(result);
+		     
+		     System.out.println();
+		     //task3
+		     System.out.println("task3");
+		     System.out.println("Hemanath");
+		     System.out.println();
+		     
+		     //task4
+		     System.out.println("task4");
+		     System.out.println("Hemanath");
+		     System.out.println("129");
+		     System.out.println();
+		     //task5
+		     System.out.println("task5");
+		     System.out.print("Hemanath"+" ");
+		     System.out.println("129");
+		     System.out.println();
+		     //task6
+		     System.out.println("task6");
+		     System.out.print("Hemanath");
+		     System.out.println();
+		     
+		     //task7
+		     System.out.println("task7");
+		     int value1=5;
+		     int value2=3;
+		     int c = value1+value2;
+		     System.out.println(" addition of 5 and 3 is " + c);
+		     
+		     
+		     System.out.println();
+		     
+		     //task8
+		     System.out.print("task8");
+		     System.out.println();
+		     System.out.println("Sir cr reddy coloege of engineering");
+		     System.out.println();
+		     
+		     //task9
+		     System.out.println("task9");
+		     System.out.println("hello 123");
+		     System.out.println();
+		     
+		     //task10
+		     System.out.println("task10");
+		     System.out.println();
+		     int a=10;
+		     int b=20;
+		     a= a/2;
+		     b=b/2;
+		     System.out.println("division of two numbers is " +  a +" "+ b);
+		   
+		     System.out.println();
+
+		   //task11
+		     System.out.println("task11");
+		    int x=10;
+		    int y= 20;
+		    int sum= x+y;
+		    System.out.println(sum);
+		    
+		    //task12
+		     System.out.println("task12");
+		     System.out.println();
+		     String movie="kancharapalem";
+		     String book="Baristar Parvathisam";
+		     System.out.println(movie);
+		   
+		     System.out.println(book);
+		     System.out.println();
+		   
+		   //task13
+		     System.out.println("task13");
+		     System.out.println();
+		     String animal= "`dog";
+		     String food= "'biryani";
+		     System.out.println(animal +" " + food);
+		     System.out.println();
+		    
+		     
+		   //task14
+		     System.out.println("task14");
+		     System.out.println();
+		     String  name ="julian`s";
+		     String  color ="green";
+		     String  foods ="pizza";
+             System.out.println(name + " favourite's color is " + color + "." + " His favourite food is  " + foods + ".");
+		     
+		     
+		     
+		     
+	 }
+
+}
